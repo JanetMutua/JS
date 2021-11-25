c625e1ac8a4333d11e741d166711febb0b835410
JanetMutua <janetmutua04@gmail.com>
2021-11-25 06:38:08 +0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+/*stylin*/
+require('styles/main.scss');
+
+/*js*/
+import $ from 'jquery';
+import { log, logTitle } from 'logger';
+
+/*my imports*/
+
+logTitle('Learning Javascript')
+
+
+/*mycode*/
+log('Fast learn');
